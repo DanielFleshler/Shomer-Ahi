626193cfc0a2d678311b13a6d755bcaa8a201a6c
Daniel Fleshler <danielflesh@my.hit.ac.il>
2025-01-12 16:31:30 +0200

    updated register-page design

--- mobileapp/pages/RegisterPage.js
+++ mobileapp/pages/RegisterPage.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, ScrollView, I18nManager } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { registerUser } from "../firebase/firebaseFunctions";
+import { Ionicons } from '@expo/vector-icons';
+
+// Force RTL layout
+I18nManager.forceRTL(true);
 
 function RegisterForm() {
     const [email, setEmail] = useState("");
@@ -12,16 +16,17 @@ function RegisterForm() {
     const [phoneNumber, setPhoneNumber] = useState("");
     const [licenseNumber, setLicenseNumber] = useState("");
     const [licensePhoto, setLicensePhoto] = useState(null);
+    const [showPassword, setShowPassword] = useState(false);
 
-    const navigator = useNavigation();
+    const navigation = useNavigation();
 
     const handleLogin = () => {
-        navigator.navigate("Login");
+        navigation.navigate("Login");
     };
 
     const handleSubmit = async () => {
         if (password !== confirmPassword) {
-            Alert.alert("Error", "Passwords do not match.");
+            Alert.alert("שגיאה", "הסיסמאות אינן תואמות.");
             return;
         }
 
@@ -37,111 +42,199 @@ function RegisterForm() {
 
         const isRegistered = await registerUser(email, password, userData);
         if (isRegistered) {
-            navigator.navigate("Login");
+            navigation.navigate("Login");
         } else {
-            Alert.alert("Error", "Registration failed.");
+            Alert.alert("שגיאה", "ההרשמה נכשלה.");
         }
     };
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>הרשמה</Text>
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.formContainer}>
+                <Text style={styles.title}>הרשמה</Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder="אימייל"
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="סיסמה"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="אימות סיסמה"
-                value={confirmPassword}
-                onChangeText={setConfirmPassword}
-                secureTextEntry
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="שם פרטי"
-                value={firstName}
-                onChangeText={setFirstName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="שם משפחה"
-                value={lastName}
-                onChangeText={setLastName}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="מספר טלפון"
-                value={phoneNumber}
-                onChangeText={setPhoneNumber}
-                keyboardType="phone-pad"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="מספר רישיון"
-                value={licenseNumber}
-                onChangeText={setLicenseNumber}
-            />
-            <TouchableOpacity style={styles.fileButton}>
-                <Text style={styles.fileButtonText}>העלה תמונת רישיון</Text>
-            </TouchableOpacity>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="שם פרטי"
+                        value={firstName}
+                        onChangeText={setFirstName}
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="person-outline" size={24} color="#999" style={styles.icon} />
+                </View>
 
-            <Button title="הרשם" onPress={handleSubmit} />
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="שם משפחה"
+                        value={lastName}
+                        onChangeText={setLastName}
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="person-outline" size={24} color="#999" style={styles.icon} />
+                </View>
 
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                <Text style={styles.loginLink} onPress={handleLogin}>?כבר יש לך חשבון התחבר</Text>
-            </TouchableOpacity>
-        </View>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="אימייל"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="mail-outline" size={24} color="#999" style={styles.icon} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="מספר טלפון"
+                        value={phoneNumber}
+                        onChangeText={setPhoneNumber}
+                        keyboardType="phone-pad"
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="call-outline" size={24} color="#999" style={styles.icon} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="מספר רישיון"
+                        value={licenseNumber}
+                        onChangeText={setLicenseNumber}
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="card-outline" size={24} color="#999" style={styles.icon} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.eyeIcon}>
+                        <Ionicons name={showPassword ? "eye-outline" : "eye-off-outline"} size={24} color="#999" />
+                    </TouchableOpacity>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="סיסמה"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry={!showPassword}
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="lock-closed-outline" size={24} color="#999" style={styles.icon} />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)} style={styles.eyeIcon}>
+                        <Ionicons name={showPassword ? "eye-outline" : "eye-off-outline"} size={24} color="#999" />
+                    </TouchableOpacity>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="אימות סיסמה"
+                        value={confirmPassword}
+                        onChangeText={setConfirmPassword}
+                        secureTextEntry={!showPassword}
+                        placeholderTextColor="#999"
+                    />
+                    <Ionicons name="lock-closed-outline" size={24} color="#999" style={styles.icon} />
+                </View>
+
+                <TouchableOpacity style={styles.fileButton}>
+                    <Text style={styles.fileButtonText}>העלה תמונת רישיון</Text>
+                    <Ionicons name="camera-outline" size={24} color="#fff" style={styles.buttonIcon} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+                    <Text style={styles.submitButtonText}>הרשם</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleLogin}>
+                    <Text style={styles.loginLink}>כבר יש לך חשבון? התחבר</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flexGrow: 1,
+        backgroundColor: "#F7F9FC",
+        alignItems: 'center',
+        justifyContent: 'center',
         padding: 20,
-        backgroundColor: "#fff",
+    },
+    formContainer: {
+        width: '100%',
+        maxWidth: 400,
     },
     title: {
-        fontSize: 24,
+        fontSize: 32,
         fontWeight: "bold",
-        marginBottom: 20,
+        marginBottom: 30,
+        color: "#333",
         textAlign: "center",
     },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#DDD',
+    },
+    icon: {
+        marginLeft: 10,
+    },
     input: {
-        borderWidth: 1,
-        borderColor: "#ccc",
+        flex: 1,
+        paddingVertical: 10,
+        fontSize: 16,
+        color: '#333',
+        textAlign: 'right',
+    },
+    eyeIcon: {
         padding: 10,
-        marginBottom: 10,
-        borderRadius: 5,
+        position: 'absolute',
+        left: 0,
+        zIndex: 1,
     },
     fileButton: {
-        backgroundColor: "#007bff",
-        padding: 10,
-        alignItems: "center",
-        borderRadius: 5,
-        marginBottom: 10,
+        backgroundColor: "#4CAF50",
+        padding: 15,
+        borderRadius: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20,
+    },
+    buttonIcon: {
+        marginLeft: 10,
     },
     fileButtonText: {
         color: "#fff",
         fontSize: 16,
+        fontWeight: "bold",
+    },
+    submitButton: {
+        backgroundColor: "#3498DB",
+        padding: 15,
+        borderRadius: 8,
+        alignItems: "center",
+        marginBottom: 20,
+    },
+    submitButtonText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
     },
     loginLink: {
-        marginTop: 20,
-        textAlign: "center",
-        color: "#007bff",
-        textDecorationLine: "underline",
+        textAlign: 'center',
+        color: "#3498DB",
+        fontSize: 16,
+        marginTop: 10,
     },
 });
 
 export default RegisterForm;
+
